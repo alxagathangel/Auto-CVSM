38849397f061e51feadc045875b33a5347ad4ec2
Alex Agathangel <158168132+alxagathangel@users.noreply.github.com>
2024-09-18 11:07:13 +0300

    Add files via upload

---
+++ passkeys.py
@@ -0,0 +1,19 @@
+input_dir = 'your_input_directory'
+output_dir = 'your_output_directory' # where the additional directories will be stored
+
+user = 'your_user'
+password = 'your_password'
+host = 'your_host'
+
+# sender email info
+email = 'your_email'
+epass = 'your_email_password'
+comp_name = 'Your Company Name'
+
+# receiver email directory with the example inputs provided
+client_emails = {
+    'HopOn1':'email@one.com',
+    'pigeon':'email@two.com',
+    'sparrow':'email@three.com',
+    'YesStore':'email@four.com'
+}
